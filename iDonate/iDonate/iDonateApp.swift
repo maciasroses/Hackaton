f7a01ff1b18cbb620782e579eab91468376b948f
sergioecv <sergiochz10@gmail.com>
2022-09-24 19:38:36 -0500

    App iniciada

---
+++ iDonate/iDonate/iDonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDonateApp.swift
+//  iDonate
+//
+//  Created by Sergio Chavez on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iDonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
