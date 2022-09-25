05a6a866c235b18216a5a192e2dcb774c043db97
sergioecv <sergiochz10@gmail.com>
2022-09-25 00:38:48 -0500

    App

---
+++ iDonate1/iDonate1/iDonate1App.swift
@@ -0,0 +1,17 @@
+//
+//  iDonate1App.swift
+//  iDonate1
+//
+//  Created by Sergio Chavez on 25/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iDonate1App: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
